a88f6fe5610ddf89a3e7931a3ed5aecbbc644384
taugk <taufiknurulhidayat12@gmail.com>
2023-12-01 09:46:29 +0700

    Modified Post Routes

--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -7,43 +7,34 @@ const ImgUpload = require("../config/imgUploadedGcs");
 const bcrypt = require("bcryptjs");
 
 router.post(
-  "/addPosts/:id_user",
+  "/addPosts",
   ImgUpload.uploadToGcs,
   ImgUpload.handleUpload,
   async (req, res) => {
     const { name, description, price, lat, lon, id_cat, is_available } =
       req.body;
-
     try {
       const authHeader = req.headers["authorization"];
       const token = authHeader && authHeader.split(" ")[1];
-
       if (!token) {
         return res.status(401).json({ message: "Unauthorized: Missing token" });
       }
-
       try {
-        // Verify the token
         const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
-
         console.log("Decoded Token:", decodedToken);
-
-        // Check if the id_user in the route matches the id_user in the token
-        if (decodedToken.id !== req.params.id_user) {
-          return res.status(403).json({ message: "Forbidden: User mismatch" });
+        if (invalidatedTokens.has(token)) {
+          return res
+            .status(401)
+            .json({ message: "Unauthorized: Token invalidated" });
         }
-
-        // Check if the category with the given id_cat exists
         const categoryExists = await Category.findByPk(id_cat);
         if (!categoryExists) {
           return res.status(404).json({ message: "Category not found" });
         }
-
         let imageUrl = "";
         if (req.file && req.file.cloudStoragePublicUrl) {
           imageUrl = req.file.cloudStoragePublicUrl;
         }
-
         const newPost = await Post.create({
           id: uuidv4(),
           name,
@@ -56,12 +47,10 @@ router.post(
           id_user: decodedToken.id,
           is_available,
         });
-
         res
           .status(201)
           .json({ message: "Post created successfully", post: newPost });
       } catch (error) {
-        // Token is invalid
         return res.status(401).json({ message: "Unauthorized: Invalid token" });
       }
     } catch (error) {
@@ -71,7 +60,7 @@ router.post(
   }
 );
 
-router.get("/All", async (req, res) => {
+router.get("/allPosts", async (req, res) => {
   try {
     const allPosts = await Post.findAll();
     res.status(200).json({ posts: allPosts });
@@ -81,29 +70,27 @@ router.get("/All", async (req, res) => {
   }
 });
 
-router.get("/users:id_users", async (req, res) => {
+router.get("/latestPosts", async (req, res) => {
   try {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
-    const decodedToken = jwt.decode(token);
-
-    console.log("Decoded Token:", decodedToken);
-
-    const userPosts = await Post.findAll({
-      where: { id_user: decodedToken.id },
+    const latestPosts = await Post.findAll({
+      order: [["createdAt", "DESC"]],
     });
-
-    res.status(200).json({ posts: userPosts });
+    res.status(200).json({ posts: latestPosts });
   } catch (error) {
-    console.error("Error fetching user posts", error);
+    console.error("Error fetching latest posts", error);
     res.status(500).json({ message: "Internal server error" });
   }
 });
 
-router.put("/updatePost/:post_id", async (req, res) => {
+router.put("/updatePost/:id", async (req, res) => {
   const { name, description, price, lat, lon, id_cat, is_available } = req.body;
 
   try {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+    console.log("Decoded Token:", decodedToken);
+
     const updatedPost = await Post.update(
       {
         name,
@@ -116,13 +103,16 @@ router.put("/updatePost/:post_id", async (req, res) => {
       },
       {
         where: {
-          id: req.params.post_id,
+          id: req.params.id,
+          id_user: decodedToken.id,
         },
       }
     );
 
     if (updatedPost[0] === 0) {
-      return res.status(404).json({ message: "Post not found" });
+      return res
+        .status(404)
+        .json({ message: "Post not found or unauthorized" });
     }
 
     res.status(200).json({ message: "Post updated successfully" });
@@ -132,17 +122,24 @@ router.put("/updatePost/:post_id", async (req, res) => {
   }
 });
 
-// Delete a post by ID
-router.delete("/deletePost/:post_id", async (req, res) => {
+router.delete("/deletePost/:id", async (req, res) => {
   try {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+    console.log("Decoded Token:", decodedToken);
+
     const deletedPost = await Post.destroy({
       where: {
-        id: req.params.post_id,
+        id: req.params.id,
+        id_user: decodedToken.id,
       },
     });
 
     if (!deletedPost) {
-      return res.status(404).json({ message: "Post not found" });
+      return res
+        .status(404)
+        .json({ message: "Post not found or unauthorized" });
     }
 
     res.status(200).json({ message: "Post deleted successfully" });
@@ -152,18 +149,30 @@ router.delete("/deletePost/:post_id", async (req, res) => {
   }
 });
 
-// Get a single post by ID
-router.get("/getPost/:post_id", async (req, res) => {
+router.get("/getPost/:id", async (req, res) => {
   try {
     const post = await Post.findByPk(req.params.post_id);
-
     if (!post) {
       return res.status(404).json({ message: "Post not found" });
     }
-
     res.status(200).json({ post });
   } catch (error) {
     console.error("Error fetching post", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.get("/postsByCategory/:id_cat", async (req, res) => {
+  try {
+    const id_cat = req.params.id_cat;
+    const postsByCategory = await Post.findAll({
+      where: {
+        id_cat: id_cat,
+      },
+    });
+    res.status(200).json({ posts: postsByCategory });
+  } catch (error) {
+    console.error("Error fetching posts by category", error);
     res.status(500).json({ message: "Internal server error" });
   }
 });
